ff0c2be2dd9b8782f16fce8f9628511b8e180789
Charlie Wall <charlie@Charlie-Walls-MacBook-Pro.local>
2020-07-10 12:22:12 -0400

    initial commit

---
+++ src/main/java/com/mcip/blockplugin/BlockPlugin.java
@@ -0,0 +1,33 @@
+package com.mcip.blockplugin;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BlockPlugin extends JavaPlugin implements Listener {
+
+  @Override
+  public void onEnable() {
+    getServer().getPluginManager().registerEvents(this, this);
+    getLogger().info("BlockPlugin enabled!");
+  }
+
+  @EventHandler
+  public void BlockEvent(PlayerInteractEvent event) {
+    Player player = event.getPlayer();
+    Action action = event.getAction();
+    if (player.hasPermission("blockpermission"))
+    if (player.getInventory().getItemInMainHand().getType() == Material.BLAZE_ROD) {
+      if (action == Action.RIGHT_CLICK_BLOCK) {
+        Material clicked = event.getClickedBlock().getType();
+        player.sendMessage(ChatColor.BLUE + "That Block is " + ChatColor.YELLOW + clicked);
+      }
+    }
+  }
+}
